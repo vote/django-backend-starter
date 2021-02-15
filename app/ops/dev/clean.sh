2b2b7155a482fb6f4b763344cdb2d8f17bfafc14
Nick Catalano <nickcatal@gmail.com>
2021-02-15 15:12:14 -0500

    Clean up cleanup

--- app/ops/dev/clean.sh
+++ app/ops/dev/clean.sh
@@ -2,6 +2,6 @@
 
 autoflake --remove-unused-variables --remove-all-unused-imports --ignore-init-module-imports --in-place --recursive --exclude /*/migrations/* /app/
 
-isort -m 3 -tc -w 88 --skip migrations --recursive /app/
+isort -m 3 -w 88 --skip migrations /app/
 
 black --exclude /*/migrations/* /app/
